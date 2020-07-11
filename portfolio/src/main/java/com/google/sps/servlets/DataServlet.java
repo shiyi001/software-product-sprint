3515708c33e514a55d4fb527dbf86d17a193c6e7
zhengliangfeng <i_am_shiyi@163.com>
2020-07-11 15:57:02 +0800

    finish week-2 step-3

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,7 +14,9 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,10 +25,34 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+  private ArrayList<String> comments;
+
+  @Override
+  public void init() {
+    comments = new ArrayList<>();
+    comments.add(
+        "A ship in port is safe, but that is not what ships are for. "
+            + "Sail out to sea and do new things. - Grace Hopper");
+    comments.add("They told me computers could only do arithmetic. - Grace Hopper");
+    comments.add("A ship in port is safe, but that's not what ships are built for. - Grace Hopper");
+    comments.add("It is much easier to apologise than it is to get permission. - Grace Hopper");
+    comments.add("If you can't give me poetry, can't you give me poetical science? - Ada Lovelace");
+    comments.add("I am in a charming state of confusion. - Ada Lovelace");
+    comments.add(
+        "The Analytical Engine weaves algebraic patterns, "
+            + "just as the Jacquard loom weaves flowers and leaves. - Ada Lovelace");
+    comments.add(
+        "Sometimes it is the people no one can imagine anything of "
+            + "who do the things no one can imagine. - Alan Turing");
+    comments.add("Those who can imagine anything, can create the impossible. - Alan Turing");
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("HELLO WORLD!");
+    Gson gson = new Gson();
+    String json = gson.toJson(comments);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
--- portfolio/src/main/webapp/js/index.js
+++ portfolio/src/main/webapp/js/index.js
@@ -1,5 +1,17 @@
 function sayHello() {
-    fetch("/data").then(response => response.text()).then((helloString) => {
-        document.getElementById('helloString').innerText = helloString;
+    fetch("/data").then(response => response.json()).then((comments) => {
+        console.log(comments);
+        const statsListElement = document.getElementById('comments-container');
+        statsListElement.innerHTML = '';
+        for (var comment_id in comments) {
+            statsListElement.appendChild(createListElement(comments[comment_id]));
+        }
     });
+}
+
+/** Creates an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
 }
