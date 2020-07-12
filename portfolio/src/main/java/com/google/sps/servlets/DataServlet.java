005d47906145358ac6d213be99e36776565006b8
Liangfeng Zheng <zhengliangfeng@pku.edu.cn>
2020-07-12 19:38:25 +0800

    Dev (#2)
    * remove css&js
    
    * add css
    
    * add js
    
    * add images
    
    * add html
    
    * add appengine-web.xml
    
    * finish step-2
    
    * finish week-2 step-3
    
    * finish week-2 step-4
    
    * finish week-2 step5

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,7 +14,16 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,10 +32,57 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+  @Override
+  public void init() {
+    saveToDataStore(
+        "A ship in port is safe, but that is not what ships are for. "
+            + "Sail out to sea and do new things. - Grace Hopper");
+    saveToDataStore("They told me computers could only do arithmetic. - Grace Hopper");
+    saveToDataStore("A ship in port is safe, but that's not what ships are built for. - Grace Hopper");
+  }
 
   @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String new_comment = request.getParameter("new_comment");
+
+    if (new_comment == null || new_comment.length() == 0) {
+      response.setContentType("text/html");
+      response.getWriter().println("Please enter an meaningful comment");
+      return;
+    } else {
+        saveToDataStore(new_comment);
+    }
+    
+    // Redirect back to the HTML page.
+    response.sendRedirect("/index.html");
+  }
+  
+  @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+    
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+    
+    ArrayList<String> comments = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+        comments.add((String)entity.getProperty("comment"));
+    }
+    
+    Gson gson = new Gson();
+    String json = gson.toJson(comments);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+  private void saveToDataStore(String comment) {
+      long timestamp = System.currentTimeMillis();
+      Entity commentEntity = new Entity("Comment");
+      commentEntity.setProperty("comment", comment);
+      commentEntity.setProperty("timestamp", timestamp);
+
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      datastore.put(commentEntity);
   }
 }
